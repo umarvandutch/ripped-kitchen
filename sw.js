41a16ad232e27c8f93ffe5e768706f477d3e9142
umarvandutch <96899121+umarvandutch@users.noreply.github.com>
2026-08-19 18:39:27 +0100

    Trigger fresh Pages deploy and refresh offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ripped-kitchen-v2';
+const CACHE='ripped-kitchen-v3';
 const CORE=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
