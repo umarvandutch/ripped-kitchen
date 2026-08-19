e4a84144a7c3260f9830ebfec6f20baa8502c1b9
umarvandutch <96899121+umarvandutch@users.noreply.github.com>
2026-08-19 20:36:26 +0100

    Cache bulk prep guide update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ripped-kitchen-v3';
-const CORE=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='ripped-kitchen-v4';
+const CORE=['./','./index.html','./styles.css','./data.js','./prep-bulk.js','./app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
