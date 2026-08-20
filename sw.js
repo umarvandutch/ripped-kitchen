dbb08315d938128cdd267b6dc8f6bb5d83d9ab75
umarvandutch <96899121+umarvandutch@users.noreply.github.com>
2026-08-20 11:53:48 +0100

    Refresh PWA cache for refined meal prep

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ripped-kitchen-v4';
+const CACHE='ripped-kitchen-v5';
 const CORE=['./','./index.html','./styles.css','./data.js','./prep-bulk.js','./app.js','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
